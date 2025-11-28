8594ec89bc7172cd476ecda2177ae81b4858ecb6
Cody Milliron <codymilliron.dev@gmail.com>
2025-11-28 12:38:25 -0500

    feat: add basic repl funcationality

--- src/repl.ts
+++ src/repl.ts
@@ -1,7 +1,27 @@
+import { stdin, stdout } from "process";
+import { createInterface } from "readline";
+
 export function cleanInput(input: string): string[] {
   const items = input.toLowerCase().trim().split(" ");
   const outputList = items.filter((i) => i.length > 0);
   return outputList;
 }
 
-export function startREPL() {}
+export function startREPL() {
+  const rl = createInterface({
+    input: stdin,
+    output: stdout,
+    prompt: "Pokedex > ",
+  });
+  rl.prompt();
+  rl.on("line", (input) => {
+    const cleanedInput = cleanInput(input);
+    if (cleanedInput.length > 0 && cleanedInput) {
+      console.log(`Your command was: ${cleanedInput[0]}`);
+    }
+    rl.prompt();
+  }).on("close", () => {
+    console.log("Pokedex closing down....");
+    process.exit();
+  });
+}
